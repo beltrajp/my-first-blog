68447994439a81795dce1eb47975c6b544b385f1
juan <beltrajp@hotmail.com>
2017-09-10 21:02:52 -0300

    vistas agregadas

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import include, url
 from . import views
+from django.conf.urls import url
 
 urlpatterns = [
-        url(r'^$', views.post_list),
+    url(r'^$', views.post_list),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
